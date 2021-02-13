8b5dac5c838cf51ab709d232d472b27834da7e5e
dangolbeeker <scottb305@outlook.com>
2021-02-12 20:45:17 -0500

    added port 51 for tor browser support

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# reddit
-reddit account/upvote script
+## Improved Reddit Mass Account Generator/Upvoting
+
+
+Original Script:
+https://github.com/ahaggard2013/Reddit-Account-Generator-and-Mass-Upvoter
+
+
+By Default the port numbers set to 9051 for configuration with the tor browser
---
+++ randomUserNames.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import random
+import string
+import names
+
+
+def generate_username_password(amount, ulength, plength, list):
+    for _ in range(0,amount):
+    	username = names.get_full_name()
+    	username = ''.join(username.split())
+        list.write(username+':'+ ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(plength)) + '\n')
+
+        
+list = open('redditNameList.txt', 'w')
+generate_username_password(100,15,15, list)
+list.close()
